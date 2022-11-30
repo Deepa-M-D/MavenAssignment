b045efcde0fd53745e141bff62d53ee33c0358a0
Deepa M d <deemd@publicisgroupe.net>
2022-11-30 09:01:24 +0530

    added

---
+++ src/test/java/com/sapient/FirstClassMaven.java
@@ -0,0 +1,14 @@
+package com.sapient;
+
+import org.testng.annotations.Test;
+
+public class FirstClassMaven {
+	@Test
+	public void main1() {
+		String firstname=" Deepa";
+		String lastname=" M D";
+		System.out.println("Hello "+firstname.concat(lastname));
+
+	}
+
+}
